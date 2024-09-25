6ae5e5c4ce5da7338b6cdded1ecf97d0a6866cb2
fakaha <lunastra34@gmail.com>
2024-09-25 23:00:50 +0700

    initial commit

---
+++ condition.js
@@ -0,0 +1,34 @@
+let answer = 4;
+
+if (answer % 2 == 0) {
+  console.log(`${answer} adalah angka genap`);
+} else {
+  console.log(`${answer} adalah angka ganjil`);
+}
+
+let hari = 1;
+switch (hari) {
+  case 1:
+    console.log("Senin");
+    break;
+  case 2:
+    console.log("selasa");
+    break;
+  case 3:
+    console.log("rabu");
+    break;
+  case 4:
+    console.log("kamis");
+    break;
+  case 5:
+    console.log("jumat");
+    break;
+  case 6:
+    console.log("sabtu");
+    break;
+  case 7:
+    console.log("minggu");
+    break;
+  default:
+    break;
+}
---
+++ variable.js
@@ -0,0 +1,37 @@
+let umur = 21;
+const nama = "Zulfa Fakaha";
+var pekerjaan = "Engineer";
+
+// Primitif Tipe data
+let greeting = "Hallo"; // String
+let jumlahKaryawan = 10; // Number
+let isHandsome = true; // Boolean
+let kegagalan = null; // Null
+let makan = undefined; //Undefined
+let symbol2 = Symbol(123); // Symbol
+
+//  Non-Primitif Tipe data
+let profile = { nama: "Zulfa Fakaha", umur: 21, alamat: "jogja" }; // Object
+let hari = ["senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"]; // Array
+
+// Operator
+let gaji = 5000000 + 5000000; // Penjumlahan
+let jobdesk = 5 - 3; // Pengurangan
+
+let a = 10;
+let b = 5;
+let luasPersegiPanjang = a * b; // Perkalian
+let luasSegitiga = (a * b) / 2; // Pembagian
+let sisaBagi = a % b; // Modulus
+
+// Operator Perbandingan
+let x = 10;
+let y = 10;
+console.log(x == y);
+console.log(x === y);
+console.log(x != y);
+console.log(x !== y);
+console.log(x > y);
+console.log(x < y);
+console.log(x <= y);
+console.log(x >= y);
