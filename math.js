5bc5beb462a7c49fd73f5e78d847ad513fc51bf1
fakaha <lunastra34@gmail.com>
2024-09-25 23:10:15 +0700

    add math

---
+++ math.js
@@ -0,0 +1,8 @@
+function luasLingkaran(jari) {
+  console.log(Math.PI * jari * jari);
+}
+luasLingkaran(10);
+
+let angka = [1, 2, 4, 8];
+const hasil = angka.map((data) => data ** 2);
+console.log(hasil);
